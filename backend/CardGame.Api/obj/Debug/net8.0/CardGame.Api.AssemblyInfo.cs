1163c8c86f8f2c190cd418e28685094e774417ba
themba <thembamaluleke6@gmail.com>
2025-12-11 17:49:45 +0200

    Add Swashbuckle.AspNetCore package for API documentation support

--- backend/CardGame.Api/obj/Debug/net8.0/CardGame.Api.AssemblyInfo.cs
+++ backend/CardGame.Api/obj/Debug/net8.0/CardGame.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardGame.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14e1e443360ce0671d8d7eb2fc7338912864a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02375f0c4790a1330db33ca2a9467d72a7290276")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardGame.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardGame.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
